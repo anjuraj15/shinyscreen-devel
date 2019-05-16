a52f17ae867221a6a003c5902dbc840383926a1f
Todor Kondić <todor.kondic@uni.lu>
2019-05-16 17:05:34 +0200

    Add rmbmix stub

---
+++ mix.R
@@ -0,0 +1,8 @@
+## Wrapper function to process data from mixed compounds.  mzml_files
+## is simply the file list, sett_file_list is the list of settings
+## corresponding to each mzML file and comp_list_noid is the csv table
+## with at least these columns of compound names (Name) and SMILES
+## _filled_.
+rmbmix<-function(mzml_files,sett_file_list,comp_list_noid) {
+    
+    }
