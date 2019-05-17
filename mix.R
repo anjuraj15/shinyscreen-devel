cd468002e85b92bdfc2c40ffb2bc84dc6d919451
Todor Kondić <todor.kondic@uni.lu>
2019-05-17 13:09:13 +0200

    Function rmbmix.single now reads in msms data
    * mix.R
    
    ** rmbmix.gen_comp_list
       Returns the number of compounds now.
    
    ** rmbmix.single
       Does the first step in the workflow. Prints out the message
       singifying whic file is currently being processed in order to
       associate with any possible warning messages. This association may
       break if we switch to parallel.

--- mix.R
+++ mix.R
@@ -24,7 +24,7 @@ rmbmix.mk_sett_file<-function(sett_alist,file) {
 ## Chemical Dashboard csv file with, at least, PREFERRED_NAMES and
 ## SMILES columns _filled_ out, or just an ordinary csv file with
 ## columns SMILES and Names filled. Argument dest_fn is the
-## destination filename.
+## destination filename. Returns the number of compounds.
 rmbmix.gen_comp_list<-function(src_fn,dest_fn) {
     df<-read.csv(src_fn)
     ## Names
@@ -36,6 +36,7 @@ rmbmix.gen_comp_list<-function(src_fn,dest_fn) {
     sz<-length(haha)
     outdf<-data.frame(Name=nms,SMILES=haha,ID=1:sz,RT=rep("",sz),CAS=rep("",sz))
     write.csv(outdf,file=dest_fn,row.names=F)
+    length(nms)
 }
     
     
@@ -43,8 +44,9 @@ rmbmix.gen_comp_list<-function(src_fn,dest_fn) {
 ## Perform the compound mixture workflow on the data file called
 ## fn_data with settings named list called sett_alist. Argument
 ## fn_cmpd_list is the compound list. Argument wd is the scratch dir
-## to hold generated ini files and the like.
-rmbmix.single<-function(fn_data,sett_alist,fn_cmpd_list,wd) {
+## to hold generated ini files and the like. Arguments mode and
+## readMethod are the same as in msmsRead.
+rmbmix.single<-function(fn_data,sett_alist,fn_cmpd_list,wd,mode,readMethod="mzR") {
     
     require(RMassBank)
 
@@ -53,13 +55,20 @@ rmbmix.single<-function(fn_data,sett_alist,fn_cmpd_list,wd) {
     rmbmix.mk_sett_file(sett_alist,sfn)
     loadRmbSettings(sfn)
 
+    ## Generate and load the compound list.
+    fn_comp<-file.path(wd,paste(fn_data,".comp.csv",sep=''))
+    n_cmpd<-rmbmix.gen_comp_list(fn_cmpd_list,fn_comp)
+    loadList(fn_comp)
+
     ## Generate file table.
     df_table<-data.frame(Files=rep(fn_data,n_cmpd),ID=1:n_cmpd)
     fn_table<-file.path(wd,paste("fn-table.",fn_data,".csv",sep=''))
     write.csv(x=df_table,file=fn_table,row.names=F)
 
- 
-
     ## Make empty workspace.
-    
+    w <- newMsmsWorkspace()
+    #w <-msmsRead(w,filetable=fn_table,readMethod="mzR",mode=mode)
+    ## Run the workflow.
+    message(paste("Reading in file:",fn_data))
+    w <-msmsRead(w,filetable=fn_table,readMethod="mzR",mode=mode)
 }
