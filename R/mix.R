dd4f0019ea0281d3b4e7e4f95927517c2f2694b0
Todor Kondić <todor.kondic@uni.lu>
2019-05-29 17:23:39 +0200

    Package with minimal functionality

---
+++ R/mix.R
@@ -0,0 +1,213 @@
+##' Create directories without drama.
+##'
+##' 
+##' @title Create directories without drama
+##' @param path Name of the directory.
+##' @return The character string containing the input argument `path`.
+##' @author Todor Kondić
+no_drama_mkdir<-function(path) {
+
+    if (! dir.exists(path)) dir.create(path)
+    path
+}
+
+##' Produce the Rmb Settings file
+##'
+##' Produce the Rmb Settings file based on the customisation file in
+##' YAML format.
+##'
+##' @title Generate RMassBank settings file.
+##' @param sett_alist The named list of settings that are different
+##'     from the RMassBank defaults.
+##' @param file The name of the YAML specification that will be merged
+##'     with the template Rmb settings file.
+##' @return NULL
+##' @author Todor Kondić
+mk_sett_file<-function(sett_alist,file) {
+    require(yaml)
+    tmp<-tempfile()
+    RMassBank::RmbSettingsTemplate(tmp)
+    sett<-yaml.load_file(tmp)
+    for (nm in names(sett_alist)) {
+        sett[[nm]]<-sett_alist[[nm]]
+    }
+    write_yaml(x=sett,file=file)
+    NULL
+}
+
+##' Generate the RMassBank compound list from the input compound list
+##' in CSV file src_fn. The input compound list format is either a
+##' Chemical Dashboard csv file with, at least, PREFERRED_ SMILES
+##' columns _filled_ out, or just an ordinary CSV file with columns
+##' SMILES and Names filled. Argument dest_fn is the destination
+##' filename. Returns the number of compounds.
+##'
+##' 
+##' @title Generate Compound List File
+##' @param src_fn The input compound list CSV filename.
+##' @param dest_fn The resulting compound list CSV filename.
+##' @return Number of compounds.
+##' @author Todor Kondić
+gen_comp_list<-function(src_fn,dest_fn) {
+    df<-read.csv(src_fn)
+    ## Names
+    nms<-if ("PREFERRED_NAME" %in% names(df)) df$PREFERRED_NAME else df$Name
+    if (is.null(nms)) stop("Unable to read compound names from the input compound list.")
+
+    ## SMILES
+    haha<-df$SMILES
+    sz<-length(haha)
+    
+    ## CAS
+    casvals<-if ("CASRN" %in% names(df)) df$CASRN else rep(NA,sz)
+    if (is.null(haha)) stop("Unable to read SMILES from the input compound list.")
+
+    outdf<-data.frame(ID=1:sz,Name=nms,SMILES=haha,CAS=casvals,RT=rep(NA,sz))
+    write.csv(outdf,file=dest_fn,row.names=F,na="")
+    length(nms)
+}
+
+##'
+##' 
+##' @title RMassBank Spectral Workflow on a Single Compound Mixture
+##' @param fn_data A mzML data file.
+##' @param stgs_alist RMassBank settings. It can either be a named
+##'     list of settings, or a filename of a YAML file.
+##' @param wd The name of the work directory.
+##' @param fn_cmpd_list The file name of he compound list
+##'     corresponding to `fn_data`.
+##' @param mode Modes as described in the standard workflow vignette
+##'     of RMassBank.
+##' @param readMethod Default read method is "mzR". Consult the
+##'     documentation of `msmsRead` for details.
+##' @param archdir The directory to store R objects created during
+##'     workflow execution.
+##' @return MsmsWorkspace object.
+##' @author Todor Kondić
+single.sw<-function(fn_data,stgs_alist,wd,fn_cmpd_list,mode,readMethod="mzR",archdir="archive") {
+    
+    require(RMassBank)
+    require(yaml)
+    ## Generate settings file and load.
+    stgs_alist<-if (is.character(stgs_alist)) yaml.load_file(stgs_alist) else stgs_alist
+    sfn<-file.path(wd,paste(fn_data,".ini",sep=''))
+    mk_sett_file(stgs_alist,sfn)
+    RMassBank::loadRmbSettings(sfn)
+
+    ## Generate and load the compound list.
+    fn_comp<-file.path(wd,paste(fn_data,".comp.csv",sep=''))
+    n_cmpd<-gen_comp_list(fn_cmpd_list,fn_comp)
+    RMassBank::loadList(fn_comp)
+
+    ## Generate file table.
+    df_table<-data.frame(Files=rep(fn_data,n_cmpd),ID=1:n_cmpd)
+    fn_table<-file.path(wd,paste("fn-table.",fn_data,".csv",sep=''))
+    write.csv(x=df_table,file=fn_table,row.names=F)
+
+    ## Make empty workspace.
+    w <- newMsmsWorkspace()
+    ## Run the workflow.
+    message(paste("Reading in file:",fn_data))
+    w <-msmsRead(w,filetable=fn_table,readMethod="mzR",mode=mode)
+    archdir<-file.path(wd,archdir)
+    if (!dir.exists(archdir)) dir.create(archdir)
+    fn_arch<-file.path(archdir,paste(fn_data,".archive",sep=''))
+    RMassBank::msmsWorkflow(w, mode=mode, steps=2:8,archivename=fn_arch)
+}
+
+
+##' Prepare single mbWorkspace object based on the workspace, the
+##' infolist name and RMassBank settings.
+##'
+##' 
+##' @title Prepare Single mbWorkspace object
+##' @param w MsmsWorkspace object.
+##' @param fn_info Filename of the infolist to be generated.
+##' @param fn_stgs Filename of the RMassBank settings.
+##' @return A mbWorkspace object.
+##' @author Todor Kondić
+mb.prep.single<-function(w,fn_info,fn_stgs) {
+    RMassBank::loadRmbSettings(fn_stgs)
+    mb <- RMassBank::newMbWorkspace(w)
+    RMassBank::resetInfolists(mb)
+    RMassBank::mbWorkflow(mb,infolist_path=fn_info)
+}
+##' Vectorize mb.prep function.
+##'
+##' 
+##' @title Vectorized mb.prep function.
+##' @param w A sequence of msmsWorkspaces.
+##' @param fn_info A sequence of infolist filenams to be generated.
+##' @param fn_stgs A sequence of settings associated with each
+##'     msmsWorkspace object.
+##' @return A list of mbWorkspaces.
+##' @author Todor Kondić
+mb.prep.v<-function(w,fn_info,fn_stgs) {
+    f<-Vectorize(mb.prep.single,vectorize.args=c("w","fn_info","fn_stgs"),SIMPLIFY=F)
+    res<-f(w,fn_info,fn_stgs)
+    names(res)<-names(w)
+    res
+}
+
+
+##' Performs a single MassBank workflow after preparation.
+##'
+##' 
+##' @title Single MassBank workflow.
+##' @param mb A mbWorkspace object.
+##' @param infodir Directory containing the infolist.
+##' @param fn_stgs The settings associated with the mbWorkspace
+##'     object.
+##' @return A mbWorkflow object.
+##' @author Todor Kondić
+mb.single<-function(mb,infodir,fn_stgs) {
+    RMassBank::loadRmbSettings(fn_stgs)
+    mb <- RMassBank::resetInfolists(mb)
+    mb <- RMassBank::loadInfolists(mb,infodir)
+    ## loadInfolists
+    ## addPeaks
+    RMassBank::mbWorkflow(mb,step=1:8)
+}
+
+stripext<-function(fn) {
+    bits<-strsplit(fn,split="\\.")[[1]]
+    if (length(bits)> 1) paste(head(bits,-1),collapse=".") else fn}
+
+
+
+##' Interface to vectorised spectral workflow.
+##'
+##' .. content for \details{} ..
+##' @title Vectorised Spectral Workflow.
+##' @param fn_data A sequence of mzML input files.
+##' @param stgs_alist A list of named list of settings, or a list of
+##'     filenames of YAML files containing the settings.
+##' @param wd The list of working directories.
+##' @param fn_cmpd_list The compound list characterising the mixtures.
+##' @param mode Same as in msmsRead.
+##' @param readMethod Same as in msmsRead.
+##' @param archdir Name of the archive.
+##' @return A named list of spectral workspaces. The names are derived
+##'     from data filenames.
+##' @author Todor Kondić
+v<-function(fn_data,stgs_alist,wd,fn_cmpd_list,mode,readMethod="mzR",archdir="archive") {
+    f<-Vectorize(single,vectorize.args=c("wd","fn_data","stgs_alist"),SIMPLIFY=F)
+    x<-f(fn_data,stgs_alist,wd,fn_cmpd_list,mode,readMethod=readMethod,archdir=archdir)
+    names(x)<-basename(fn_data)
+    x}
+    
+##' Interface to vectorised Mass Bank workflow.
+##'
+##' .. content for \details{} ..
+##' @title Vectorised Mass Bank Workflow
+##' @param mb List of mass bank workflow objects
+##' @param infodir List of subdirs containing info lists.
+##' @param fn_stgs List of settings files.
+##' @return A named list of mbWorkspace objects. The names are derived
+##'     from the input mb sequence.
+##' @author Todor Kondić
+mb.v<-function(mb,infodir,fn_stgs) {
+    f<-Vectorize(mb.single,vectorize.args=c("mb","infodir","fn_stgs"),SIMPLIFY=F)
+    x<-f(mb,infodir,fn_stgs)
+    names(x)<-names(mb)
+    x}
---
+++ R/run.R
@@ -0,0 +1,81 @@
+##' Paste with no separator.
+##'
+##' .. content for \details{} ..
+##' @title Paste With No Separator
+##' @param ... Strings to paste together.
+##' @return Pasted string.
+##' @author Todor Kondić
+attch<-function(...) paste(...,sep='')
+
+
+##' Performs massbank workflow on multiple mzML files:
+##'
+##'
+##'  1. Create rdir if it does not exist.
+##'
+##'  2. Generate data subdirectories inside `rdir` that will be used to
+##'  keep the processed data belonging to a particular mzML file in
+##'  the fileset.
+##' 
+##'  3. Generate the full RMassBank settings files by merging the
+##'  sections from the `fn_cmpd_list` with the defaults and place those
+##'  new settings files in the appropriate data subdirs.
+##'
+##'  4. Finally perform the spectral workflow on each data file and
+##'  place the results in the data subdirs.
+##' 
+##' @title Perform MassBank Workflow on Multiple Compound Mixtures
+##' @param fn_data List of mzML data filenames to be processed.
+##' @param fn_cmpd_list Compound list.
+##' @param mode as in msmsRead.
+##' @param rdir The root data directory.
+##' @return A named list of msmsWorkspace objects.
+##' @author Todor Kondić
+##' @export
+sw.do<-function(fn_data,fn_cmpd_list,mode,rdir=".") {
+ 
+    mkdir(rdir)
+    wdirs<-sapply(basename(fn_data),function(nm) file.path(rdir,stripext(nm)))
+    sapply(wdirs,mkdir)
+    stgs<-sapply(basename(wdirs),function (nm) paste(nm,"yml",sep='.'))
+    v(fn_data,stgs,wdirs,fn_cmpd_list,mode)
+}
+
+##' Creates and prepares mbWorkspace objects before the full workflow
+##' is performed on them. In process, create directory `info` as a
+##' subdir of any particular data dir and place the starting info list
+##' there.
+##'
+##' 
+##' @title Prepare mbWorkspace objects
+##' @param w A list of spectral workspace inputs.
+##' @param rdir Data root.
+##' @return Named list of prepared mbWorkspace objects.
+##' @author Todor Kondić
+##' @export
+mb.prep<-function(w,rdir=".") {
+    idir<-function(n) file.path(rdir,stripext(n))
+    sapply(names(w),function (n) mkdir(file.path(idir(n),"info")))
+    fn_info<-sapply(names(w),function (n) file.path(idir(n),"info",attch(n,'.info.csv')))
+    fn_stgs<-sapply(names(w),function(n) file.path(idir(n),attch(n,'.ini')))
+    mb.prep.v(w,fn_info,fn_stgs)
+}
+
+
+##' Perform the Mass Bank workflow on the prepared mbWorkspace
+##' objects. FOR THE RECORD: writing documentation is boring. Anyway,
+##' record results in the info subfolder of each data subdir.
+##'
+##' .. content for \details{} ..
+##' @title Perform the Mass Bank workflow
+##' @param mb The list of prepared mbWorkspace objects.
+##' @param rdir Root data dir.
+##' @return The named list of processed mbWorkspace objects.
+##' @author Todor Kondić
+##' @export
+mb.do<-function(mb,rdir=".") {
+    idir<-function(n) file.path(rdir,stripext(n))
+    infodir<-sapply(names(mb),function(n) file.path(idir(n),"info"))
+    fn_stgs<-sapply(names(mb),function(n) file.path(idir(n),attch(n,'.ini')))
+    mb.v(mb,infodir,fn_stgs)
+}
