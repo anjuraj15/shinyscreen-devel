8cecce50f39a24a00a16f92d134f73653d722e46
Todor Kondić <todor.kondic@uni.lu>
2019-12-04 11:50:48 +0100

    Add resources.R
    This is where constants go.

---
+++ R/resources.R
@@ -0,0 +1,31 @@
+## Constants
+FN_FTAB_BASE<-"ftable.base.csv"
+FN_FTAB_PP<-"ftable.pp.csv"
+FN_PP_OUT_PREF<-"PP.filetable"
+FN_FTAB<-"ftable.csv"
+FN_CMP_L<-"compounds.csv"
+FN_LOC_SETID <-"setid.csv"
+FN_COMP_TAB<-"comprehensive.csv"
+MODEMAP<-list(pH="MpHp_mass",
+              mH="MmHm_mass",
+              pNH4="MpNH4_mass",
+              pNa="MpNa_mass")
+
+DEFAULT_RT_RANGE=c(NA,NA)
+
+QANAMES <- c("MS1","MS2","Alignment","AboveNoise")
+PLOT_DEF_TAGS<-NA
+PLOT_DEF_SET<-NA
+
+CEX<-0.75
+RT_DIGITS=2
+M_DIGITS=4
+PAL="Dark2"
+
+REST_TXT_INP<-c("fnTgtL",
+                "fnUnkL",
+                "fnSetId",
+                "tagsInp",
+                "confFileTabBase",
+                "confFileTabProcInp",
+                "confResFileTab")
