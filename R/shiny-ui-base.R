3323f5eec2643bdebdd1e43b9488f712c478cb3e
Todor Kondić <todor.kondic@uni.lu>
2020-05-02 10:59:02 +0200

    Fix importFrom directive
    * R/shiny-ui-base.R, NAMESPACE:
      importFrom(shiny,validate) now correct.

--- R/shiny-ui-base.R
+++ R/shiny-ui-base.R
@@ -12,7 +12,7 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
-##' @importFrom(shiny, validate)
+##' @importFrom shiny validate
 
 react_v <- shiny::reactiveValues
 react_f <- shiny::reactive
