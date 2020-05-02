a0b384906dd7cc0df2fcebcbdeb5aaa3d1d5aa87
Todor Kondić <todor.kondic@uni.lu>
2020-05-02 07:10:59 +0200

    Return cwd from volumes also
    * R/shiny-ui-base.R(volumes): Attach the current working directory.

--- R/shiny-ui-base.R
+++ R/shiny-ui-base.R
@@ -21,7 +21,7 @@ obsrv <- shiny::observe
 obsrv_e <- shiny::observeEvent
 vols <- shinyFiles::getVolumes
 vol_f <- vols()
-volumes <- shinyFiles::getVolumes()
+volumes <- function() c(wd=getwd(), shinyFiles::getVolumes()())
 validate1 <- function(expr,msg) shiny::validate(need(expr,msg))
 
 
