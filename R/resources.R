331c288ab47402a4ff67d4ac8bb89aad09d5eef5
Todor Kondić <todor.kondic@uni.lu>
2020-05-05 12:56:36 +0200

    Rename parameter EMPTY_UNK -> EMPTY_UNKNOWN
    * R/resources.R(EMPTY_UNK): -> EMPTY_UNKNOWN

--- R/resources.R
+++ R/resources.R
@@ -116,7 +116,7 @@ FTAB_KEY=c("set","tag","mz")
 FTAB_NAMES=c("ID", "mz", "rt", "tag", "mode", "set", "Name", "SMILES", "Files" , "wd","origin")
 
 
-EMPTY_UNK <- dtable(ID=character(0),mz=numeric(0),RT=numeric(0),Name=character(0),CAS=character(0))
+EMPTY_UNKNOWN <- dtable(ID=character(0),mz=numeric(0),RT=numeric(0),Name=character(0),CAS=character(0))
 EMPTY_KNOWN <- dtable(ID=character(0),SMILES=character(0),RT=numeric(0),Name=character(0),CAS=character(0))
 
 ## Trivial data table
